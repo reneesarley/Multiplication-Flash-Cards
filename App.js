28985c30db7e9436a107b2b20e38166a2e7421de
Renee Sarley <reneesarley@gmail.com>
2018-09-11 07:02:13 -0700

    add buttons

--- App.js
+++ App.js
@@ -1,16 +1,51 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Button } from 'react-native-elements';
 import Game from './Game';
 
 
 
 type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component<{}> {
+
+  // _startGame(){
+  //   this.props.navigator.push({
+  //     title: 'Game',
+  //     component: Game
+  //     });
+  // };
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to Flash Cards</Text>
-        <Text style={styles.instructions}>Get Started</Text>
+        <Button
+          title='Start Game'
+          buttonStyle= {{
+            backgroundColor: "rgba(92, 99,216, 1)",
+            width: 300,
+            height: 45,
+            borderColor: "rgb(127,255,0)",
+            borderWidth: 0,
+            borderRadius: 5
+          }}
+        />
+        <Button
+          title="LOADING BUTTON"
+          loading
+          loadingProps={{ size: "large", color: "rgba(111, 202, 186, 1)" }}
+          titleStyle={{ fontWeight: "700" }}
+          buttonStyle={{
+            backgroundColor: "rgba(92, 99,216, 1)",
+            width: 300,
+            height: 45,
+            borderColor: "transparent",
+            borderWidth: 0,
+            borderRadius: 5
+          }}
+          containerStyle={{ marginTop: 20 }}
+        />
+
       </View>
     );
   }
@@ -33,4 +68,12 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  startButton:{
+    backgroundColor: "rgba(92, 99,216, 1)",
+    width: 300,
+    height: 45,
+    borderColor: "transparent",
+    borderWidth: 0,
+    borderRadius: 5
+  }
 });
--- game.js
+++ game.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
 
-export default class App extends Component<Props> {
+export default class Game extends Component<{}> {
+  constructor(props){
+    super(props);
+  }
   render() {
     return (
       <View style={styles.container}>
